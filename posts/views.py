a01368abca7a456e7066d8f493c9a02b264fecf4
Brightone Ochieng <brightone777@gmail.com>
2025-06-29 13:49:58 +0300

    Fix indentation and clean up views

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,15 @@
 import os
-from django.http import JsonResponse
-from django.http import HttpResponse
+import logging
+from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
 from django.contrib.auth import get_user_model
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views.decorators.http import require_POST
 from django.contrib.admin.views.decorators import staff_member_required
 from .models import Post
 from .forms import PostForm
+
+logger = logging.getLogger(__name__)
 
 # Helper function to get client IP address
 
@@ -18,7 +19,7 @@ def get_client_ip(request):
     return x_forwarded_for.split(',')[0] if x_forwarded_for else request.META.get('REMOTE_ADDR')
 
 
-# Homepage - show all unblocked posts
+# Homepage - show all unblocked posts and allow anonymous posting
 def home(request):
     posts = Post.objects.filter(blocked=False).order_by('-created_at')
     form = PostForm()
@@ -28,11 +29,10 @@ def home(request):
         if form.is_valid():
             if request.FILES.get('image'):
                 from django.core.files.storage import default_storage
-            import logging
-            logger = logging.getLogger(__name__)
-            logger.info("Image upload detected.")
-            logger.info("Storage backend: %s", default_storage)
-            logger.info("Uploaded file name: %s", request.FILES['image'].name)
+                logger.info("Image upload detected.")
+                logger.info("Storage backend: %s", default_storage)
+                logger.info("Uploaded file name: %s",
+                            request.FILES['image'].name)
 
             post = form.save(commit=False)
             post.user = request.user if request.user.is_authenticated else None
@@ -43,18 +43,17 @@ def home(request):
     return render(request, 'posts/all_posts.html', {'posts': posts, 'form': form})
 
 
-# Dedicated create view (optional if using home)
+# Optional separate anonymous post creation page
 def create_anonymous_post(request):
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             if request.FILES.get('image'):
                 from django.core.files.storage import default_storage
-            import logging
-            logger = logging.getLogger(__name__)
-            logger.info("Image upload detected.")
-            logger.info("Storage backend: %s", default_storage)
-            logger.info("Uploaded file name: %s", request.FILES['image'].name)
+                logger.info("Image upload detected.")
+                logger.info("Storage backend: %s", default_storage)
+                logger.info("Uploaded file name: %s",
+                            request.FILES['image'].name)
 
             post = form.save(commit=False)
             post.user = request.user if request.user.is_authenticated else None
@@ -63,15 +62,16 @@ def create_anonymous_post(request):
             return redirect('post_success')
     else:
         form = PostForm()
+
     return render(request, 'posts/create_anonymous_post.html', {'form': form})
 
 
-# Post creation success page
+# Success page after posting
 def post_success(request):
     return render(request, 'posts/post_success.html')
 
 
-# Admin or public post list
+# Post list view for admin and public users
 def post_list(request):
     if request.user.is_staff:
         posts = Post.objects.all().order_by('-created_at')
@@ -80,7 +80,7 @@ def post_list(request):
     return render(request, 'posts/post_list.html', {'posts': posts})
 
 
-# Block/unblock a post
+# Admin: block or unblock a post
 @require_POST
 @staff_member_required
 def toggle_block(request, post_id):
@@ -90,7 +90,7 @@ def toggle_block(request, post_id):
     return HttpResponseRedirect(reverse('post_list'))
 
 
-# Delete a post
+# Admin: delete a post
 @require_POST
 @staff_member_required
 def delete_post(request, post_id):
@@ -99,6 +99,7 @@ def delete_post(request, post_id):
     return HttpResponseRedirect(reverse('post_list'))
 
 
+# Check if Cloudinary settings are loaded correctly (for debugging)
 def test_cloudinary_config(request):
     return JsonResponse({
         "CLOUD_NAME": os.environ.get("CLOUDINARY_CLOUD_NAME"),
